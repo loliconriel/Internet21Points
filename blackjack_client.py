e508420f266d9aad3c6be4ca70257639c7baedcc
nameless0629 <115072747+nameless0629@users.noreply.github.com>
2024-11-28 14:45:40 +0800

    Update blackjack_client.py

--- blackjack_client.py
+++ blackjack_client.py
@@ -3,22 +3,26 @@ import socket
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("127.0.0.1", 8888))
+    game_started = False
 
     while True:
         data = client.recv(1024).decode()
         print(data)
 
-        if "加入房間" in data:
-            room = input("請輸入房間號碼: ")
+        if "加入房間" in data and not game_started:
+            room = input()
             client.sendall(room.encode())
 
         elif "是否加牌" in data:
-            choice = input(data)
+            game_started = True
+            choice = input()
             client.sendall(choice.encode())
 
         elif "是否重新開始" in data:
-            choice = input(data)
+            choice = input()
             client.sendall(choice.encode())
+            if choice.lower() == 'n':
+                game_started = False
 
 if __name__ == "__main__":
     main()
